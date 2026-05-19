6d9b7c9bea6d2b6caa43df0d8e182cb83288f966
Guilherme <guilhermesb095@gmail.com>
2026-05-19 12:26:41 -0300

    feat: implement SellerDao findAll method

--- src/model/dao/impl/SellerDAOJDBC.java
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -130,6 +130,41 @@ public class SellerDAOJDBC implements SellerDAO {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName "+
+                    "FROM seller INNER JOIN department "+
+                    "ON seller.DepartmentId = department.Id "+
+                    "ORDER BY Name");
+
+            rs = st.executeQuery();
+
+            List<Seller> sellerList = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()){
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if(dep == null){
+                    dep = createDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller seller = createSeller(rs, dep);
+                sellerList.add(seller);
+            }
+
+            return sellerList;
+
+        }catch (SQLException e){
+            throw new ExceptionDB(e.getMessage());
+        }finally {
+            DB.closeStatment(st);
+            DB.closeResultSet(rs);
+
+        }
     }
 }
