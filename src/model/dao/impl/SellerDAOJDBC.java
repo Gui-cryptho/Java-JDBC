e4a11e2834c198b6ad176788fc161021869b4722
Guilherme <guilhermesb095@gmail.com>
2026-05-19 15:43:05 -0300

    feat: implementation SellerDao deleteById method

--- src/model/dao/impl/SellerDAOJDBC.java
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -87,6 +87,21 @@ public class SellerDAOJDBC implements SellerDAO {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement("DELETE FROM seller "+
+                    "WHERE Id = ?");
+
+            st.setInt(1, id);
+            st.executeUpdate();
+
+
+        }catch (SQLException e){
+            throw new ExceptionDB(e.getMessage());
+        }finally {
+            DB.closeStatment(st);
+        }
 
     }
 
